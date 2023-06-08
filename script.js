ff081ab2d3f5dc475f768e3c80de121921e36242
Gabriel Fonseca <gabrielligadaso220@gmail.com>
2023-06-08 13:35:18 -0300

    redução de código no script

--- script.js
+++ script.js
@@ -1,29 +1,5 @@
-class MobileNavbar {
-  constructor(mobileMenu, navList, navLinks) {
-    this.mobileMenu = document.querySelector(mobileMenu);
-    this.navList = document.querySelector(navList);
-    this.navLinks = document.querySelectorAll(navLinks);
-    this.activeClass = "active";
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    console.log(this);
-    this.navList.classList.toggle(this.activeClass);
-  }
-
-  addClickEvent() {
-    this.mobileMenu.addEventListener("click", this.handleClick);
-  }
-
-  init() {
-    if (this.mobileMenu) {
-      this.addClickEvent();
-    }
-    return this;
-  }
+//Código otimizado:
+function recuarMenu() {
+  const nav = document.querySelector("nav");
+  nav.classList.toggle("ativo");
 }
-
-const mobileNavbar = new MobileNavbar(".mobile-menu", ".lista", ".lista li");
-mobileNavbar.init();
